a11b3969aea77415b17f3af82974420eec5abaec
Lazoangel <a.23308051281190@cbtis128.edu.mx>
2024-09-27 05:33:57 -0600

    Add files via upload

---
+++ Lazo_Angel_1190_3W_6.py
@@ -0,0 +1,5 @@
+print ("Lazo Jimenez Angel Jesus 3W")
+b = float (input("ingresar el valor de la base de el rectangulo: ")) #Pide ingresar la base del rectangulo
+h = float (input("ingresar el valor de la altura de el rectangulo: ")) #Pide ingresar la altura del rectangulo
+perimetro = (b+h) * 2 #Muestra la operacion para sacar el perimetro del rectangulo 
+print (f"el perimetro de el rectangulo es {perimetro}") #Se muestra el resultado final del perimetro
